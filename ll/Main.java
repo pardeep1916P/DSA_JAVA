9c345fb075cf70b051478de5c4eca6d028126576
pardeep1916P <chaitanyacharan07@gmail.com>
2025-10-08 21:56:06 +0530

    java_dsa_01

---
+++ ll/Main.java
@@ -0,0 +1,11 @@
+package src.ll;
+
+public class Main {
+    public static void main(String[] args){
+        LinkedList myLinkedList=new LinkedList(3);
+        myLinkedList.printList();    //3
+        myLinkedList.getHead();      //3
+        myLinkedList.getTail();      //3
+        myLinkedList.getLength();    //1
+    }
+}

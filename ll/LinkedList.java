f459372f967947771f52b334787fa97116488bb9
pardeep1916P <chaitanyacharan07@gmail.com>
2025-10-09 12:27:12 +0530

    ll_append

--- ll/LinkedList.java
+++ ll/LinkedList.java
@@ -11,12 +11,16 @@ public class LinkedList{
     private Node head;
     private Node tail;
     private int length;
+    
+    //ll constructor
     public LinkedList(int value){
         Node newNode =new Node(value);
         head=newNode;
         tail=newNode;
         length=1;
     }
+    
+    //print list
     public void printList(){
         if(head==null){
             System.out.println("Empty");
@@ -32,6 +36,8 @@ public class LinkedList{
         }
         System.out.println();
     }
+    
+    //print head value
     public void getHead(){
         if (head==null) {
             System.out.println("Empty");
@@ -39,6 +45,8 @@ public class LinkedList{
             System.out.println("Head :" + head.value);
         }
     }
+    
+    //print tail value
     public void getTail(){
         if (tail==null) {
             System.out.println("Empty");
@@ -46,7 +54,104 @@ public class LinkedList{
             System.out.println("Tail :" + tail.value);
         }
     }
+    
+    //print length
     public void getLength(){
             System.out.println("Length :" + length);
+    }
+    
+    //append a value (add at last) using tail
+    public void append(int value){
+        Node newNode =new Node(value);
+        if(head==null){
+            head = newNode;
+            tail = newNode;
+        } else{
+            tail.next=newNode;
+            tail=tail.next;
+        }
+        length++;
+    }
+
+    //append a value (add at last) without using tail
+    public void append_without_tail(int value){
+        Node newNode = new Node(value);
+        if( head ==null){
+            head=newNode;
+        }else{
+            Node temp=head;
+            while(temp.next!=null){
+                temp=temp.next;
+            }
+            temp.next=newNode;
+        }
+        length++;
+    }
+
+    //prepend a value (add at first)
+    public void prepend(int value){
+        Node newNode =new Node(value);
+        if(head==null){
+            head=newNode;
+        }else{
+            newNode.next=head;
+            head=newNode;
+        }
+        length++;
+    }
+
+    //remove first node
+    public Node removeFirst(){
+        Node temp=head;
+        if(head==null) return null;
+        else if (head == tail) {
+            head=null;
+            tail=null;
+        }else{
+            head=head.next;
+            temp.next=null;
+        }
+        length--;
+        return temp;
+    }
+
+    //remove last node
+    public Node removeLast(){
+        Node temp=head;
+        if(head==null){
+            return null;
+        } else if (head==tail) {
+            head=null;
+            tail=null;
+        }else{
+            while(temp.next!=tail){
+                temp=temp.next;
+            }
+            tail=temp;
+            temp=temp.next;
+            tail.next=null;
+        }
+        length--;
+        return temp;
+    }
+
+    // get node by index
+    public Node get(int index){
+        if(index<0 || index>length) return null;
+        Node temp= head;
+        for(int i=0;i<index;i++){
+            temp=temp.next;
+        }
+        return temp;
+    }
+
+    //set value
+    public boolean set(int index,int value){
+        Node temp= get(index);
+        if (temp!=null){
+            temp.value=value;
+            return true;
+        }
+        return false;
     }
 }
--- ll/Main.java
+++ ll/Main.java
@@ -2,10 +2,20 @@ package src.ll;
 
 public class Main {
     public static void main(String[] args){
-        LinkedList myLinkedList=new LinkedList(3);
-        myLinkedList.printList();    //3
-        myLinkedList.getHead();      //3
-        myLinkedList.getTail();      //3
-        myLinkedList.getLength();    //1
+        System.out.println("set:");
+        LinkedList ll1=new LinkedList(1);
+        LinkedList ll2=new LinkedList(5);
+        ll1.append(2);
+        ll1.append(3);
+        ll2.append(6);
+        ll2.append(7);
+        ll2.prepend(4);
+        ll1.prepend(0);
+        ll1.printList();
+        System.out.println("setting index 2 value to 99: "+ll1.set(2,99));
+        ll1.printList();
+        ll2.printList();
+        System.out.println("setting index 1 value to 99: "+ll2.set(1,99));
+        ll2.printList();
     }
 }

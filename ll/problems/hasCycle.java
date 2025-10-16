ab3501493143c46584ec73ca73a1bd30892d67e2
pardeep1916P <chaitanyacharan07@gmail.com>
2025-10-16 19:10:16 +0530

    ll_problem16

--- ll/problems/hasCycle.java
+++ ll/problems/hasCycle.java
@@ -1,4 +1,12 @@
 package src.ll.problems;
 
-public class hasCycle {
+public boolean hasCycle(ListNode head) {
+    if(head==null || head.next==null) return false;
+    ListNode fast=head,slow=head;
+    while(fast!=null && fast.next!=null){
+        fast=fast.next.next;
+        slow=slow.next;
+        if(fast==slow) return true;
+    }
+    return false;
 }

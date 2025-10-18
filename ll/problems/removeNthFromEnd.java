b32ab7d838867cffdd1f351aeddd3ea8fa37313e
pardeep1916P <chaitanyacharan07@gmail.com>
2025-10-18 16:25:35 +0530

    ll_problem20

--- ll/problems/removeNthFromEnd.java
+++ ll/problems/removeNthFromEnd.java
@@ -1,4 +1,14 @@
 package src.ll.problems;
 
-public class removeNthFromEnd {
+public ListNode removeNthFromEnd(ListNode head, int n) {
+    if(head==null) return null;  //if list is null
+    ListNode slow=head,fast=head;
+    for(int i=0;i<n;i++) fast=fast.next; //distance between fast and slow is n
+    if(fast==null) return head.next;    //if fast is null head is the nth node to be deleted
+    while(fast!=null && fast.next!=null){
+        fast=fast.next;
+        slow=slow.next;
+    }
+    slow.next=slow.next.next; //slow is before nth node
+    return head;
 }
